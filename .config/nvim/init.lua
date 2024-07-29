24934c4de4b8c37a25b128968bafa13ad34f261b
Armand Touminet <accounts@atouminet.fr>
2024-07-29 17:10:18 +0200

    telescope jumplist

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -421,7 +421,60 @@ local plugins = {
     {
         "lewis6991/gitsigns.nvim",
         config = function()
-            require("gitsigns").setup()
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local gitsigns = require("gitsigns")
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map("n", "]c", function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ "]c", bang = true })
+                        else
+                            gitsigns.nav_hunk("next")
+                        end
+                    end)
+
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({ "[c", bang = true })
+                        else
+                            gitsigns.nav_hunk("prev")
+                        end
+                    end)
+
+                    -- Actions
+                    map("n", "<leader>hs", gitsigns.stage_hunk)
+                    map("n", "<leader>hr", gitsigns.reset_hunk)
+                    map("v", "<leader>hs", function()
+                        gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end)
+                    map("v", "<leader>hr", function()
+                        gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                    end)
+                    map("n", "<leader>hS", gitsigns.stage_buffer)
+                    map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+                    map("n", "<leader>hR", gitsigns.reset_buffer)
+                    map("n", "<leader>hp", gitsigns.preview_hunk)
+                    map("n", "<leader>hb", function()
+                        gitsigns.blame_line({ full = true })
+                    end)
+                    map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+                    map("n", "<leader>hd", gitsigns.diffthis)
+                    map("n", "<leader>hD", function()
+                        gitsigns.diffthis("~")
+                    end)
+                    map("n", "<leader>td", gitsigns.toggle_deleted)
+
+                    -- Text object
+                    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+                end,
+            })
         end,
     },
     {
@@ -465,6 +518,7 @@ local plugins = {
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>fj", builtin.jumplist, {})
         end,
     },
     "nvim-telescope/telescope-dap.nvim",
@@ -537,7 +591,8 @@ local plugins = {
             })
         end,
     },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    { "catppuccin/nvim",            name = "catppuccin", priority = 1000 },
+    { "shortcuts/no-neck-pain.nvim" },
 }
 require("lazy").setup(plugins)
 
